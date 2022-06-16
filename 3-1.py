0c0f06f642992e641259280c45de039c9c1c177a
mryazdan <mrrezayazdani@yahoo.com>
2022-06-16 10:15:32 +0430

    S3

---
+++ 3-1.py
@@ -0,0 +1,45 @@
+# print(help(len))
+
+# print(len('reza'))
+# print(len([1, 2, 3, 4]))
+# print(len((1, 2)))
+# print(len({2, 2, 2, 2, 3, 3, 4, 5, 6, 7, 4}))
+
+names = ['ramin', 'ali', 'akbar', 'mohammad']
+# print(list(enumerate(names)))
+
+# for index, name in enumerate(names):
+#     print(f"{index}. {name}")
+
+
+# Start calculator
+# input : 3 * 6
+# result : 18
+
+# def calc(num1, num2, operator):
+#     if operator == "*":
+#         return num1 * num2
+#     elif operator == '/':
+#         return num1 / num2
+#     elif operator == '+':
+#         return num1 + num2
+#     return num1 - num2
+#
+#
+# n1, op, n2 = input().split()
+# n1 = float(n1)
+# n2 = float(n2)
+#
+# print(calc(n1, n2, op))
+
+
+# print(eval(input())) Calculator
+
+# eval(input())
+
+# name = exec("import os; os.name")
+# print(name)
+
+# exec(input())
+# print(name)
+
