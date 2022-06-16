f1bf36b718055fc7b4e1cf43d180919df0661513
mryazdan <mrrezayazdani@yahoo.com>
2022-06-16 11:22:57 +0430

    S3

---
+++ 3-2.py
@@ -0,0 +1,52 @@
+# in-place operator
+
+# a = [1, 2]
+# b = a
+
+# a += b
+# a = a + b
+
+# print(id(a))
+# print(id(b))
+
+
+# print(a, b, sep='\n')
+
+# number = int(input('Enter a number: '))
+#
+# print(n := number)
+# print(id(n), id(number), sep='\n')
+
+# if (n := number) and number > 0:
+#     print(n, 'is positive !')
+
+# print(False == False in [False])
+# print((False == False) and (False in [False]))
+
+# print(1 > 0 < 1)
+# 1 > 0 and 0 < 1
+
+# Cpython zen of id in memory
+# print("145 :", id(145))
+# print("145 :", id(145))
+# print("-4 :", id(-4))
+# print("-4 :", id(-4))
+# print("-4 :", id(-4))
+# print("288 :", id(288))
+# print("288 :", id(288))
+# print("288 :", id(288))
+# print("288 :", id(288))
+
+# infinity = float('inf')
+# nan = float('nan')  # Not a number
+
+# while True:
+#     n = int(input('Enter a number :'))
+#     # print(n > infinity)
+#     print(n > nan)
+
+# print('Akbar' is not None)
+# print('Akbar' is (not None)) # False
+
+
+
